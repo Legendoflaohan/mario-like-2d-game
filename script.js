fcb2d65cecc7b4c98dd1f200f3f860a8a7c0f786
Legendoflaohan <115490550+Legendoflaohan@users.noreply.github.com>
2023-04-25 20:48:13 +0800

    Make cube stop if touch the bottom of the canvas

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ const c = canvas.getContext('2d');
 canvas.width = 1024
 canvas.height = 576
 
-//when you const a new cube, it has Cube's properties and does initialize.
+// when you const a new cube, it has Cube's properties and does initialize.
 class Cube {
-    //Set little properties for the class
-    //Constructor is let declaring variables, and set values to them.
+    // Set little properties for the class
+    // Constructor is let declaring variables, and set values to them.
     constructor(anchorPoint, speed, acc) {
-        //this refers to this class, so this.initialize() refers to the function below.
-        //this.anchorPoint is given whatever the argument passes.
+        // this refers to this class, so this.initialize() refers to the function below.
+        // this.anchorPoint is given whatever the argument passes.
         this.anchorPoint = anchorPoint;
+        // Initial the width and height of the cube.
+        this.width = 100;
+        this.height = 100;
         //set initial speed and acc.
         this.speed = speed;
         this.acc = acc;
@@ -20,28 +23,35 @@ class Cube {
     //Action one: generate a blue cube.
     initialize() {
         c.fillStyle = 'blue';
-        c.fillRect(this.anchorPoint.x, this.anchorPoint.y, 200, 200);
+        c.fillRect(this.anchorPoint.x, this.anchorPoint.y, this.width, this.height);
     }
 
     move() {
+        // Read this if statement after the rest part of this function.
+        if (this.anchorPoint.y + this.height >= canvas.height) {
+            // Fix the 误差
+            this.anchorPoint.y = canvas.height - this.height;
+            // Stop the cube.
+            this.speed = 0;
+        }
         //move the blue cube downwards 1px per fps.
         this.anchorPoint.y += this.speed;
         //acceleration
         this.speed += this.acc;
     }
 }
-//Set a cube with class Cube. Set it's anchorPoint, speed, and acc.
-const cube = new Cube({ x: 100, y: 100, }, 1, 0.5);
-//Animation loop function.
+// Set a cube with class Cube. Set it's anchorPoint, speed, and acc.
+const cube = new Cube({ x: 400, y: 0, }, 0, 9.8);
+// Animation loop function.
 function hangHang() {
-    //A per fpx refresh purple background for the blue cubes.
+    // A per fpx refresh purple background for the blue cubes.
     c.fillStyle = 'purple';
     c.fillRect(0, 0, canvas.width, canvas.height);
-    //cube who just born will execute initialize function in class Cube, each fps it move 1px downwards.
+    // cube who just born will execute initialize function in class Cube, each fps it move 1px downwards.
     cube.initialize();
-    //cube who just born will execute move function in class Cube, for each fps it executes one time.
+    // cube who just born will execute move function in class Cube, for each fps it executes one time.
     cube.move();
     window.requestAnimationFrame(hangHang);
 }
-//execute hangHang.
+// execute hangHang.
 hangHang();
