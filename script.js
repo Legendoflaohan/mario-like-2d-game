4caa10453ff56e163e544bf6eac9d08c9a7b9124
Legendoflaohan <115490550+Legendoflaohan@users.noreply.github.com>
2023-04-25 10:20:19 +0800

    Update speed accelerate changeable feature with OOP.

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-canvas.width = 1024;
-canvas.height = 576;
+canvas.width = 1024
+canvas.height = 576
 
-//Read this line after finish the animate function.
-//Declare y outside cuz otherwize the box and the value
-// would be set to 100 every fps fresh like a lime.
+//when you const a new cube, it has Cube's properties and does initialize.
+class Cube {
+    //Set little properties for the class
+    //Constructor is let declaring variables, and set values to them.
+    constructor(anchorPoint, speed, acc) {
+        //this refers to this class, so this.initialize() refers to the function below.
+        //this.anchorPoint is given whatever the argument passes.
+        this.anchorPoint = anchorPoint;
+        //set initial speed and acc.
+        this.speed = speed;
+        this.acc = acc;
+    }
+    //Actions of the class, actions can use the constructor properties above
+    //Action one: generate a blue cube.
+    initialize() {
+        c.fillStyle = 'blue';
+        c.fillRect(this.anchorPoint.x, this.anchorPoint.y, 200, 200);
+    }
 
-let y = 100;
-let speed = 1;
-let acc = 1;
-//a loop that refresh in every fps, by default it's 60 fps
-//body oh body do I love this stuff!!
-//It's sooooo smooth!!!!
-function animate() {
-    //refresh the canvas with color white in every fps.
-    c.fillStyle = 'white';
-    c.fillRect(0, 0, canvas.width, canvas.height);
-    //Here is the red cube.
-    c.fillStyle = 'red';
-    c.fillRect(100, y, 200, 200);
-    //Red cube move 1px downwards per fps.
-    y += speed;
-    speed += acc;
-    window.requestAnimationFrame(animate);
+    move() {
+        //move the blue cube downwards 1px per fps.
+        this.anchorPoint.y += this.speed;
+        //acceleration
+        this.speed += this.acc;
+    }
 }
-
-animate(); 
+//Set a cube with class Cube. Set it's anchorPoint, speed, and acc.
+const cube = new Cube({ x: 100, y: 100, }, 1, 0.5);
+//Animation loop function.
+function hangHang() {
+    //A per fpx refresh purple background for the blue cubes.
+    c.fillStyle = 'purple';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    //cube who just born will execute initialize function in class Cube, each fps it move 1px downwards.
+    cube.initialize();
+    //cube who just born will execute move function in class Cube, for each fps it executes one time.
+    cube.move();
+    window.requestAnimationFrame(hangHang);
+}
+//execute hangHang.
+hangHang();
