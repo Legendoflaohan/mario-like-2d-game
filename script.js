64ff0bd1aee51b8cc7190b369256346d523f1347
Legendoflaohan <115490550+Legendoflaohan@users.noreply.github.com>
2023-04-26 22:28:20 +0800

    Update background pic.

--- script.js
+++ script.js
@@ -15,8 +15,11 @@ class Player {
     }
 
     initialize() {
-        c.fillStyle = 'grey';
+        c.fillStyle = 'red';
         c.fillRect(this.anchorPoint.x, this.anchorPoint.y, this.width, this.height);
+
+        c.fillStyle = 'brown';
+        c.fillRect(this.anchorPoint.x, this.anchorPoint.y, this.width, this.height / 2);
     }
 
     gravity() {
@@ -46,13 +49,40 @@ class Player {
     }
 
     // 封装。
-    execution() {
-        player.initialize();
+    execute() {
+        this.initialize();
         // Make jump before gravity set this.anchorPoint.y to fix number.
-        player.jump();
-        player.move();
-        player.gravity();
+        this.jump();
+        this.move();
+        this.gravity();
     }
+}
+
+class Sprite {
+    // Use {} for the argument of constructor is to benefit form the key: value form,
+    // for it can illustrate the name of the argument we are manipulating, provides solid readability.
+    // Down to the code which const background to check it out.
+    constructor({ anchorPoint, imageSrc }) {
+        // Just like the Player class.
+        this.anchorPoint = anchorPoint;
+        // Create a instance of Image() class, then this.image can use the stuff inside the built-in class: Image().
+        this.image = new Image();
+        // Set the image src.
+        this.image.src = imageSrc;
+    }
+    // Introduce a way to draw image on the canvas.
+    paint() {
+        // Read the rest before read this line.
+        // This line of code provides errors, if this.image doesn't exists, return.
+        if (!this.image) {return;}
+        // canvas 2d API.
+        c.drawImage(this.image, this.anchorPoint.x, this.anchorPoint.y)
+    }
+
+    execute() {
+        this.paint();
+    }
+
 }
 
 const keys = {
@@ -63,13 +93,32 @@ const keys = {
         pressed: false
     },
 }
-
+// Introduce a player.
+// It's called a new instance.
 const player = new Player({ x: 450, y: 0, }, { h: 0, v: 0 }, 9.8);
+// Introduce the background.
+const background = new Sprite ({
+    anchorPoint: {
+        x: 0,
+        y: 0,
+    },
+    imageSrc: './super-mario-background.jpg',
+})
 
 function animation() {
     c.fillStyle = 'wheat';
     c.fillRect(0, 0, canvas.width, canvas.height);
-    player.execution();
+    // Read next line of code then go back here.
+    // Here's the solution, use canvas save and restore to create an box,
+    // only stuff inside the box will be affected by scale method~~~
+    c.save();
+    // Use canvas scale method to adjust the size of image.
+    // But there's a problem, the scale method keeps working every loop, so we need a solution.
+    c.scale(0.1333, 0.1333);
+    background.execute();
+    c.restore();
+
+    player.execute();
     window.requestAnimationFrame(animation);
 }
 
@@ -83,7 +132,7 @@ window.addEventListener('keydown', (e) => {
         case 'ArrowLeft':
             keys.ArrowLeft.pressed = true;
             break;
-        case 'ArrowUp': 
+        case 'ArrowUp':
             player.velocity.v = -25;
             break;
     }
